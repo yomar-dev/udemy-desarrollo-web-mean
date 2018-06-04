80940451edece8cfc424fe340719f710a247ed83
Yomar Miranda <mirandayomar@gmail.com>
2018-06-04 17:20:06 -0500

    Guardar artista en la BD

--- controllers/artist.js
+++ controllers/artist.js
@@ -11,6 +11,28 @@ function getArtist(req, res){
 	res.status(200).send({ message: 'Metodo getArtist() del controlador Artists.js' });
 }
 
+function saveArtist(req, res){
+	var artist = new Artist();
+
+	var params = req.body;
+	artist.name = params.name;
+	artist.description = params.description;
+	artist.image = 'null';
+
+	artist.save((err, artistStored) => {
+		if(err){
+			res.status(500).send({ message: 'Error al guardar el artista' });
+		}else{
+			if(!artistStored){
+				res.status(404).send({ message: 'El artista no he ido guardado' });
+			}else{
+				res.status(200).send({ artist: artistStored });
+			}
+		}
+	});
+}
+
 module.exports = {
-	getArtist
+	getArtist,
+	saveArtist
 }
