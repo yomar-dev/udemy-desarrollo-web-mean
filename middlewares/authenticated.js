f1c9245609e11c515df78bb5afba7e9ff99d9f83
Yomar Miranda <mirandayomar@gmail.com>
2018-06-04 13:13:53 -0500

    Middleware para comprobar si el usuario se encuentra autenticado

---
+++ middlewares/authenticated.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'clave_secreta';
+
+exports.ensureAuth = function(req, res, next){
+	if(!req.headers.authorization){
+		return res.status(403).send({ message: 'La petición no tiene la cabecera de autenticación.' });
+	}
+
+	var token = req.headers.authorization.replace(/['"]+/g, '');
+
+	try{
+		var payload = jwt.decode(token, secret);
+
+		if(payload.exp <= moment().unix()){
+			return res.status(401).send({ message: 'Su token ha expirado' });
+		}
+	}catch(ex){
+		console.log(ex);
+		return res.status(404).send({ message: 'Token no valido' });
+	}
+
+	req.user = payload;
+	next();
+}
