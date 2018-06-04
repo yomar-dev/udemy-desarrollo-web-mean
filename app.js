f58bce42da6a44f8d971384556e8ea6bee6d30d1
Yomar Miranda <mirandayomar@gmail.com>
2018-06-04 17:01:48 -0500

    Ruta base para acceder a los artistas

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 
 // Cargar rutas
 var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -15,5 +16,6 @@ app.use(bodyParser.json());
 
 // Rutas base
 app.use('/api', user_routes);
+app.use('/api', artist_routes);
 
 module.exports = app;
