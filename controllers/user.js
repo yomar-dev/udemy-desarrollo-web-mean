04fc975ffb87f74e126887cf9068bfc0f2f59f70
Yomar Miranda <mirandayomar@gmail.com>
2018-06-04 11:51:13 -0500

    Guardar usuario en la BD

--- controllers/user.js
+++ controllers/user.js
@@ -25,8 +25,29 @@ function saveUser(req, res){
 		/**
 		 * Encriptar el password
 		 */
+		bcrypt.hash(params.password, null, null, function(err, hash){
+			user.password = hash;
+			if(user.name != null && user.surname != null && user.email != null){
+				/**
+				 * Guardar usuario en la BD
+				 */
+				user.save((err, userStored) => {
+					if(err){
+						res.status(500).send({ message: 'Error al guardar el usuario' });
+					}else{
+						if(!userStored){
+							res.status(404).send({ message: 'El usuario no ha sido registrado' });
+						}else{
+							res.status(200).send({ user: userStored });
+						}
+					}
+				});
+			}else{
+				res.status(200).send({ message: 'Debes llenar todos los campos' });
+			}
+		});
 	}else{
-		res.status(500).send({ message: 'Introduce un password!' })
+		res.status(200).send({ message: 'Introduce un password!' });
 	}
 }
 
